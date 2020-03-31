419c60da9217cfad2301c75563c2b090ad3b34f0
AbkrSlh <59297531+AbkrSlh@users.noreply.github.com>
2020-03-31 13:08:42 +0800

    initit

---
+++ build/precache-manifest.b0c08c403bd090f0b5664fa9e448bf6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979daa0813779f76b270e33e63520910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53c2cafb32102d074e71",
+    "url": "/static/css/main.f1ee9527.chunk.css"
+  },
+  {
+    "revision": "e1a452469bc71b7403ab",
+    "url": "/static/js/2.7ab315b8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7ab315b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c2cafb32102d074e71",
+    "url": "/static/js/main.df0f1e0b.chunk.js"
+  },
+  {
+    "revision": "7f31f04669a7e0c04c51",
+    "url": "/static/js/runtime-main.59d46d1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
